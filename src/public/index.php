975bcd164db7b9896d4161475c4e4302831ac1b2
WebweDesign <a_attar.morey@webwedesign.eu>
2014-10-01 16:48:55 +0200

    idem que avant

--- src/public/index.php
+++ src/public/index.php
@@ -23,6 +23,6 @@
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="assets/jquery/dist/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
+    <script src="assets/bootstrap/dist/js/bootstrap.min.js"></script>
   </body>
 </html>
