bcc7b025c5964f9ff6822aa5528e1b227644007f
WebweDesign <a_attar.morey@webwedesign.eu>
2014-10-03 14:36:15 +0200

    adding stuff

--- src/public/index.php
+++ src/public/index.php
@@ -23,6 +23,7 @@
   </head>
   <body>
     <h1>Hello, world!</h1>
+    <h1><?php echo 'Hello World'; ?></h1>
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="assets/jquery/dist/jquery.min.js"></script>
