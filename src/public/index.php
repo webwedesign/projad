198d7f8fb7571bce5a51b2ad2ccd386a22a03e16
WebweDesign <a_attar.morey@webwedesign.eu>
2014-10-06 14:08:03 +0200

    testing

--- src/public/index.php
+++ src/public/index.php
@@ -1,5 +1,5 @@
 <?php 
-    require '../../vendor/autoload.php';
+//    require '../../vendor/autoload.php';
 ?>
 
 <!DOCTYPE html>
@@ -23,7 +23,90 @@
   </head>
   <body>
     <h1>Hello, world!</h1>
-    <h1><?php echo 'Hello World'; ?></h1>
+    <h1><?php echo 'Hello World ';   ?></h1>
+    
+    
+    <!-- <fieldset> // regroupant des inputs
+    <input>
+    <input>
+    <input> -->
+    <form class="form-inline" role="form" method="get">
+  <div class="form-group">
+    <label class="sr-only" for="exampleInputEmail2">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Enter email">
+  </div>
+  <div class="form-group">
+    <div class="input-group">
+      <div class="input-group-addon">@</div>
+      <input class="form-control" type="email" placeholder="Enter email">
+    </div>
+  </div>
+  <div class="form-group">
+    <label class="sr-only" for="exampleInputPassword2">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password">
+  </div>
+  <div class="checkbox">
+    <label>
+      <input type="checkbox"> Remember me
+    </label>
+  </div>
+  <button type="submit" class="btn btn-default">Sign in</button>
+   <br>
+  <div class="radio">
+  <label>
+    <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>
+    Option one is this and that&mdash;be sure to include why it's great
+  </label>
+</div>
+ <br>
+<div class="radio">
+  <label>
+    <input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
+    Option two can be something else and selecting it will deselect option one
+  </label>
+</div>
+ <br>
+<div class="radio disabled">
+  <label>
+    <input type="radio" name="optionsRadios" id="optionsRadios3" value="option3" disabled>
+    Option three is disabled
+  </label>
+  <br>
+  <select class="form-control">
+  <option>Fiat</option>
+  <option>Peugeot</option>
+  <option>Lada</option>
+  <option>Renault</option>
+  <option>Seat</option>
+</select>
+<br>
+<select multiple class="form-control">
+  <option>Fiat</option>
+  <option>Peugeot</option>
+  <option>Lada</option>
+  <option>Renault</option>
+  <option>Seat</option>
+</select>
+<br>
+
+</form>
+ <br>
+    
+    input
+        -text  
+        -password
+        -submit
+        -checkbox
+        -radio
+        -hidden
+        -.... // faire attention selon les navigateurs
+        
+     select
+        option
+        optgroup
+      
+      
+  
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="assets/jquery/dist/jquery.min.js"></script>
---
+++ src/public/test1.php
@@ -0,0 +1,12 @@
+<?php echo 'balise standard'
+//<? 'short open tag'
+//<% echo 'ASP style'
+//<?= 'raccourci de echo'?>
+<?php
+header('toto : 1234');
+
+echo ' test ';
+
+header('toto : 1234');
+
+?>
