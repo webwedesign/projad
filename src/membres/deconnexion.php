8adf77623d6367ff465ed4ecb59251ac5e462584
WebweDesign <a_attar.morey@webwedesign.eu>
2014-10-13 17:32:11 +0200

    connexion deconexion added

---
+++ src/membres/deconnexion.php
@@ -0,0 +1,41 @@
+<?php
+/*
+
+Page deconnexion.php
+
+Permet de se déconnecter du site.
+
+Quelques indications : (Utiliser l'outil de recherche et rechercher les mentions données)
+
+Liste des fonctions :
+--------------------------
+Aucune fonction
+--------------------------
+
+
+Liste des informations/erreurs :
+--------------------------
+Déconnexion
+--------------------------
+*/
+
+session_start();
+include('../functions/config.php');
+include('../functions/fonctions.php');
+connexionbdd();
+mysql_query("DELETE FROM connectes WHERE connectes_id = ".$_SESSION['membre_id']) or exit(mysql_error());
+vider_cookie();
+session_destroy();
+
+$informations = Array(/*Déconnexion*/
+				false,
+				'Déconnexion',
+				'Vous êtes à présent déconnecté.',
+				' - <a href="'.ROOTPATH.'/membres/connexion.php">Se connecter</a>',
+				ROOTPATH.'/index2.php',
+				5
+				);
+
+require_once('../information.php');
+exit();
+?>
